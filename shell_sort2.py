761317c3e6d21435a24549155b0039a04e13a64d
Torin Nelson <nelsontorin1@gmail.com>
2024-04-16 16:45:07 -0700

    reduced overhead of shell sort to improve time complexity

--- shell_sort2.py
+++ shell_sort2.py
@@ -8,9 +8,8 @@ def shell_sort2(nums):
     k = int(math.log(n, 2))
     sequence = [1, 3, 5, 9, 17, 33, 65, 129, 257, 513, 1025, 2049, 4097, 8193, 16385, 32769, 65537, 131073, 262145, 524289, 1048577, 2097153, 4194305, 8388609, 16777217, 33554433, 67108865, 134217729, 268435457, 536870913, 1073741825, 2147483649]
     sequence = sequence[:k]
-    sequence.reverse()
     pass
-    for gap in sequence:
+    for gap in reversed(sequence):
         for i in range(gap, n):
             temp = nums[i]
             j = i
